000ef3fc8f68a38fd33f0387cbf6cf566b76b506
AwaPreciousNkeng <awaprecious3t@gmail.com>
2026-05-04 18:57:59 +0100

    Configuring the intial application

---
+++ src/main/java/com/codewithpcodes/cardiag/CarDiagApplication.java
@@ -0,0 +1,13 @@
+package com.codewithpcodes.cardiag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDiagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarDiagApplication.class, args);
+    }
+
+}
